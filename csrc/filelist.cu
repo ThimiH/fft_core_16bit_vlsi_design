c27f5131ed0c042beba3df7ae4d3a90fe61644f4
ThimiH <e19276@eng.pdn.ac.lk>
2025-06-22 23:00:30 +0530

    add vcs test case 2

--- csrc/filelist.cu
+++ csrc/filelist.cu
@@ -4,20 +4,12 @@ ifeq ($(LDVERSION), 0)
 endif
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _22513_archive_1.so
-_22513_archive_1.so : archive.3/_22513_archive_1.a
+ARCHIVE_OBJS += _26748_archive_1.so
+_26748_archive_1.so : archive.0/_26748_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../sim/vcs/output/fft_simv.daidir//_22513_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../sim/vcs/output/fft_simv.daidir//_26748_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../sim/vcs/output/fft_simv.daidir//_22513_archive_1.so $@
-
-
-ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.3/_prev_archive_1.a
-	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../sim/vcs/output/fft_simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
-	@rm -f $@
-	@ln -sf .//../sim/vcs/output/fft_simv.daidir//_prev_archive_1.so $@
+	@ln -sf .//../sim/vcs/output/fft_simv.daidir//_26748_archive_1.so $@
 
 
 
